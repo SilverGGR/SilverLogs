c5fd4904ca701940a9451f6de5f9b807b7433058
Ruven Radicke <ruven.radicke@bechtle.com>
2025-05-15 10:49:21 +0200

    backendProject

---
+++ SilverLogsBackend/src/main/java/com/SilverGGR/SilverLogs/SilverLogsApplication.java
@@ -0,0 +1,13 @@
+package com.SilverGGR.SilverLogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverLogsApplication.class, args);
+	}
+
+}
---
+++ SilverLogsBackend/src/test/java/com/SilverGGR/SilverLogs/SilverLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SilverGGR.SilverLogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
